4a60876f84633a60c6fb94db00a2f538eb7b0104
dogamur <63543255+dogamur@users.noreply.github.com>
2023-04-02 23:46:27 +0300

    Update imgeditor.py

--- pillow/imgeditor.py
+++ pillow/imgeditor.py
@@ -1,14 +1,20 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
+# get user input for image name
+img_name = input("Enter the name of the image file (including extension): ")
+
+# define input and output paths
 path = r"C:\Users\murabıt\Desktop"
 pathOut = r"C:\Users\murabıt\Desktop\editedImgs"
 
 if not os.path.exists(pathOut):
     os.mkdir(pathOut)
 
-img = Image.open(f"{path}/img.jpg")
-
+# open and edit the image
+img = Image.open(f"{path}/{img_name}")
 edit = img.filter(ImageFilter.SHARPEN)
-clean_name = os.path.splitext("img.jpg")[0]
+clean_name = os.path.splitext(img_name)[0]
+
+# save the edited image
 edit.save(f"{pathOut}/{clean_name}_edited.jpg")
